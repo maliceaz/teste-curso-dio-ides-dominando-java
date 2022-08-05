e2bffa930ade92b4a72a10fff245c8c4e11b490d
maliceaz <110574796+maliceaz@users.noreply.github.com>
2022-08-05 18:43:44 -0300

    Update PrimeiroPrograma.java

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,6 +9,9 @@ public class PrimeiroPrograma {
 		Gato gato = new Gato();
 		Livros livros = new Livros(); 
 		
+		System.out.println(gato);
+		System.out.println(livros);
+		
 		/*int a = 2;
 		int b = 3;
 		System.out.println("Hello World! " + (a+b));*/
